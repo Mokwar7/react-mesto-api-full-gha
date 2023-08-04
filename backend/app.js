e176f6bd11a232f5e77e781a1f3978bfcf1bcab2
Mokwar <mokwar@MacBook-Air-Mokwar.local>
2023-08-04 14:54:26 +0300

    sad

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const auth = require('./middlewares/auth');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
